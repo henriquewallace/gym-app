2cf45485e9275afa65e1effdfc0b986fa4238e44
henriquewallace <henrique.wallace@townsq.com.br>
2023-03-07 00:52:43 -0300

    GYM - Add custom spacing and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,31 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    colors: {
+      'primary': '#F15152',
+      'primary-90': '#F26263',
+      'primary-80': '#F47475',
+      'black': '#464646',
+      'black-50': '#A2A2A2',
+      'black-30': '#C7C7C7',
+      'black-10': '#ECECEC',
+      'black-5': '#F6F6F6',
+      'white': '#FFFFFF',
+    },
+    extend: {
+      spacing: {
+        '4': '4px',
+        '8': '8px',
+        '16': '16px',
+        '24': '24px',
+        '32': '32px',
+        '40': '40px',
+        '48': '48px',
+        '56': '56px',
+        '64': '64px',
+        '72': '72px',
+      },
+    },
+    plugins: [],
+  }
 }
